17e6d32dc95d9ea35ab6b85ceff79a54fc066fda
Pontus <pontus.z94@gmail.com>
2023-12-04 17:59:24 +0100

    name unique logic and list feedback EP

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,17 +1,30 @@
 const express = require("express")
 const router = express.Router()
 const { dashboardWidget, feedback } = require("../db/models")
+const { Op } = require("sequelize")
 
 router.post("/widget-create", async (req, res) => {
-  try {
-    await dashboardWidget.create({
-      data: req.body.data,
+  const uniqueName = await dashboardWidget.findOne({
+    where: {
+      name: {
+        [Op.iLike]: req.body.name,
+      },
       dashboardId: req.body.dashboardId,
-      name: req.body.name,
-    })
-    res.status(201).json({ message: "Resource created successfully" })
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong" })
+    },
+  })
+  if (!uniqueName) {
+    try {
+      await dashboardWidget.create({
+        data: req.body.data,
+        dashboardId: req.body.dashboardId,
+        name: req.body.name,
+      })
+      res.status(201).json({ message: "Resource created successfully" })
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" })
+    }
+  } else {
+    res.status(409).json({ message: "Name already exists" })
   }
 })
 router.get("/list-widgets", async (req, res) => {
@@ -21,6 +34,25 @@ router.get("/list-widgets", async (req, res) => {
       where: {
         dashboardId: dashboardId,
       },
+    })
+
+    res.status(200).send(data)
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" })
+  }
+})
+router.get("/list-feedback", async (req, res) => {
+  let { dashboardId, name } = req.query
+  try {
+    const data = await dashboardWidget.findAll({
+      attributes: ["id", "name"],
+      where: {
+        dashboardId: dashboardId,
+        name: {
+          [Op.iLike]: name,
+        },
+      },
+      include: feedback,
     })
 
     res.status(200).send(data)
